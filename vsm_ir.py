857e243265f2cbe9c2e6f33b32b6918c8cd2ee8a
Rotem Brooks <rotembr10@gmail.com>
2021-07-12 19:05:13 +0300

    step a1

--- vsm_ir.py
+++ vsm_ir.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import json
 import os
 from lxml import etree
@@ -7,24 +8,43 @@ from nltk.corpus import stopwords
 
 nltk.download('stopwords')
 
-FREQ = "Frequency"
+TF = "tf"
 TF_IDF = "tf-idf"
 inverted_index = {}
 
 
 def build_vocabulary(path):
+    files_number = 0
     for file in os.listdir(path):
         root = etree.parse(path + "\\" + file)
-        parse_file(root)
+        files_number += parse_file(root, files_number)
+    calc_tf_idf(files_number)
+    json_file = json.dumps(inverted_index)
+    with open("vsm_inverted_index.json", "w") as outfile:
+        outfile.write(json_file)
+    outfile.close()
 
 
-def parse_file(root):
+def calc_tf_idf(files_number):
+    for word in inverted_index:
+        print(word)
+        n = len(inverted_index[word][TF])
+        idf = math.log(files_number / n, 2)
+        for doc in inverted_index[word][TF]:
+            inverted_index[word][TF_IDF] = {doc: inverted_index[word][TF][doc] * idf}
+
+
+def parse_file(root, files_number):
     for record in root.xpath(".//RECORD"):
-        record_num = (record.xpath("./RECORDNUM/text()")).lstrip("0").rstrip()
+        files_number += 1
+        record_num = (record.xpath("./RECORDNUM/text()"))[0].lstrip("0").rstrip()
         text = ""
-        text += record.xpath(".//TITLE/text()") + " "
-        text += record.xpath(".//EXTRACT/text()") + " "
-        text += record.xpath(".//ABSTRACT/text()") + " "
+        if len(record.xpath(".//TITLE/text()")) > 0:
+            text += record.xpath(".//TITLE/text()")[0] + " "
+        if len(record.xpath(".//EXTRACT/text()")) > 0:
+            text += record.xpath(".//EXTRACT/text()")[0] + " "
+        if len(record.xpath(".//ABSTRACT/text()")) > 0:
+            text += record.xpath(".//ABSTRACT/text()")[0] + " "
 
         tokens = [word.lower() for word in text.split(" ") if word != ""]
 
@@ -44,20 +64,19 @@ def parse_file(root):
 
         for token in tokens_wo_stopwords:
             if token in inverted_index:
-                if record_num in inverted_index[token]:
-                    inverted_index[token][record_num][FREQ] += 1
-                else:
-                    inverted_index[token][record_num] = {FREQ: 1}
+                if record_num not in inverted_index[token]:
+                    inverted_index[token][record_num] = {TF: tf[token]}
             else:
-                inverted_index[token] = {record_num: {FREQ: 1}}
+                inverted_index[token] = {record_num: {TF: tf[token]}}
+    return files_number
 
 
 def main():
     args = sys.argv
     if args[1] == 'create_index':
         build_vocabulary(args[2])
-    elif args[1] == 'query':
-        ask_question(args[2], 'ontology.nt')
+    # elif args[1] == 'query':
+    #     ask_question(args[2], 'ontology.nt')
 
 
 if __name__ == "__main__":
